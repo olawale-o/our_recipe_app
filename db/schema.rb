7e17ecc43cf2420a54504bba96796178e597a16c
olawale-o <omoogunolawale@gmail.com>
2021-11-15 17:42:00 +0100

    Add recipe_foods_count to fix n+1 queries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_140232) do
+ActiveRecord::Schema.define(version: 2021_11_15_163930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_140232) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "recipe_foods_count", default: 0
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
