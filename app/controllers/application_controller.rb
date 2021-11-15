e28ef2341a0af79ac2f96fc97a93898c9c1baf8e
Simon Grchevski <simongrcevski@gmail.com>
2021-11-15 15:20:46 +0100

    Modify the Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  rescue_from CanCan::AccessDenied do |_exception|
+    redirect_to root_url, alert: 'You are either not logged in or not authorized to access that resource'
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
