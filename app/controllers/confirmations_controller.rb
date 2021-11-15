1addbec3bdd543e0af2322afbb5cba76713ee08f
Simon Grchevski <simongrcevski@gmail.com>
2021-11-15 15:19:03 +0100

    Add after confirmation for signup

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,2 +1,8 @@
-class ConfirmationsController < ApplicationController
+class ConfirmationsController < Devise::ConfirmationsController
+  private
+
+  def after_confirmation_path_for(_resource_name, resource)
+    sign_in(resource)
+    foods_path
+  end
 end
