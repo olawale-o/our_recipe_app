0b199c16305effe2f1db659217a2dd7607e05190
olawale-o <omoogunolawale@gmail.com>
2021-11-16 13:16:44 +0100

    Modify model specs

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:recipe) { build(:recipe) }
+    it 'name should be present' do
+      recipe.name = nil
+      expect(recipe).to_not be_valid
+    end
+
+    it 'preparation_time should be present' do
+      recipe.preparation_time = nil
+      expect(recipe).to_not be_valid
+    end
+
+    it 'cooking_time should be present' do
+      recipe.cooking_time = nil
+      expect(recipe).to_not be_valid
+    end
+
+    it 'description should be present' do
+      recipe.description = nil
+      expect(recipe).to_not be_valid
+    end
+  end
 end
